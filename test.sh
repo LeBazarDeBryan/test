68a530e86fbf22601b4d8597f407162a9d7b2000
Le Bazar de Bryan <85786075+LeBazarDeBryan@users.noreply.github.com>
2023-10-22 13:07:11 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -23,9 +23,9 @@ sudo docker run -d \
 
 printf 'Starting up pihole container '
 for i in $(seq 1 20); do
-    if [ "$(docker inspect -f "{{.State.Health.Status}}" pihole)" == "healthy" ] ; then
+    if [ "$(sudo docker inspect -f "{{.State.Health.Status}}" pihole)" == "healthy" ] ; then
         printf ' OK'
-        echo -e "\n$(docker logs pihole 2> /dev/null | grep 'password:') for your pi-hole: http://${IP}/admin/"
+        echo -e "\n$(sudo docker logs pihole 2> /dev/null | grep 'password:') for your pi-hole: http://${IP}/admin/"
         exit 0
     else
         sleep 3
@@ -33,7 +33,7 @@ for i in $(seq 1 20); do
     fi
 
     if [ $i -eq 20 ] ; then
-        echo -e "\nTimed out waiting for Pi-hole start, consult your container logs for more info (\`docker logs pihole\`)"
+        echo -e "\nTimed out waiting for Pi-hole start, consult your container logs for more info (\`sudo docker logs pihole\`)"
         exit 1
     fi
 done;
